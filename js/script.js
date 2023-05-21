8574c94bc6ca4430b1e2e877cccb0c484eba8fae
Jim2510 <guglielmino2510@gmail.com>
2023-05-21 14:16:04 +0200

    risolto errore display 0000

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const decrementBtn = document.getElementById("decrement");
 const inputNumber = document.getElementById('value');
 const addButton = document.getElementById('addValue');
 const remButton = document.getElementById('remValue');
-let counter = 0;
+let counter = 0000;
 
 // Event listener per il pulsante di incremento
 incrementBtn.addEventListener("click", () => {
@@ -36,13 +36,13 @@ addButton.addEventListener('click', () => {
     const numberToAdd = Number(inputNumber.value);
     const newValue = isNaN(numberToAdd) ? currentValue : currentValue + numberToAdd;
     counter = newValue > 9999 ? 9999 : newValue;
-    count.textContent = counter;
+    count.textContent = counter.toString().padStart(4, '0');
 });
 
 // Event listener per l'input del pulsante di aggiunta con gestione del valore non numerico
 addButton.addEventListener('input', () => {
     if (isNaN(parseInt(inputNumber.value))) {
-        count.textContent = 0;
+        count.textContent = 0000;
     }
 });
 
@@ -52,17 +52,17 @@ remButton.addEventListener('click', () => {
     const numberToRem = Number(inputNumber.value);
     const newValue = isNaN(numberToRem) ? currentValue : currentValue - numberToRem;
 
-    if (newValue < 0) {
+    if (newValue < 0000) {
         return;
     }
 
     counter = newValue;
-    count.textContent = counter;
+    count.textContent = counter.toString().padStart(4, '0');
 });
 
 // Event listener per l'input del pulsante di rimozione con gestione del valore non numerico
 remButton.addEventListener('input', () => {
     if (isNaN(parseInt(inputNumber.value))) {
-        count.textContent = 0;
+        count.textContent = 0000;
     }
 });
