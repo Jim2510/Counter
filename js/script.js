8667558c687485bed78b2e3c2629e7aa102b14e2
Jim2510 <guglielmino2510@gmail.com>
2023-05-21 13:51:12 +0200

    -Aggiunte funzioni che impediscono al counter di mostrare un numero negativo e di superare il limite di 9999; -Modifica allo stile della pagina; -Commento codice js;

--- js/script.js
+++ js/script.js
@@ -6,44 +6,63 @@ const addButton = document.getElementById('addValue');
 const remButton = document.getElementById('remValue');
 let counter = 0;
 
+// Event listener per il pulsante di incremento
 incrementBtn.addEventListener("click", () => {
-    counter++;
-    count.textContent = counter;
-});
-
-decrementBtn.addEventListener("click", () => {
-    if (counter > 0) {
-        counter--;
-        count.textContent = counter;
+    if (counter < 9999) {
+        counter++;
+        count.textContent = counter.toString().padStart(4, '0');
     }
 });
 
+// Event listener per il pulsante di decremento
+decrementBtn.addEventListener("click", () => {
+    if (counter > 0) {
+        counter--;
+        count.textContent = counter.toString().padStart(4, '0');
+    }
+});
+
+// Event listener per il pulsante di decremento con gestione della prevenzione dell'evento
+decrementBtn.addEventListener("click", (event) => {
+    if (counter === 0) {
+        event.preventDefault();
+    }
+});
+
+
+// Event listener per il pulsante di aggiunta
 addButton.addEventListener('click', () => {
     const currentValue = counter;
     const numberToAdd = Number(inputNumber.value);
-    const newValue = isNaN(numberToAdd) ? currentValue : currentValue + numberToAdd ;
-    counter = newValue;
+    const newValue = isNaN(numberToAdd) ? currentValue : currentValue + numberToAdd;
+    counter = newValue > 9999 ? 9999 : newValue;
     count.textContent = counter;
-    
 });
 
+// Event listener per l'input del pulsante di aggiunta con gestione del valore non numerico
 addButton.addEventListener('input', () => {
     if (isNaN(parseInt(inputNumber.value))) {
         count.textContent = 0;
-        }
+    }
 });
 
+// Event listener per il pulsante di rimozione
 remButton.addEventListener('click', () => {
     const currentValue = counter;
     const numberToRem = Number(inputNumber.value);
     const newValue = isNaN(numberToRem) ? currentValue : currentValue - numberToRem;
+
+    if (newValue < 0) {
+        return;
+    }
+
     counter = newValue;
     count.textContent = counter;
 });
 
+// Event listener per l'input del pulsante di rimozione con gestione del valore non numerico
 remButton.addEventListener('input', () => {
     if (isNaN(parseInt(inputNumber.value))) {
         count.textContent = 0;
     }
 });
-
