de16b28da051c1dcacdd7a99efa5304368a488c4
Jim2510 <guglielmino2510@gmail.com>
2023-06-05 15:07:23 +0200

    Modifica visualizzazione pulsanti che adesso vengono generati nel file script.js e richiamati con appendChild nei container di riferimento; Aggiunto Event Delegation al posto degli event listener; modifica stile e aggiunta animazioni

--- js/script.js
+++ js/script.js
@@ -1,75 +1,77 @@
 const count = document.getElementById("count");
-const incrementBtn = document.getElementById("increment");
-const decrementBtn = document.getElementById("decrement");
 const inputNumber = document.getElementById('value');
-const addButton = document.getElementById('addValue');
-const remButton = document.getElementById('remValue');
 const resetBtn = document.getElementById('reset');
-let counter = 0000;
+const btns = document.getElementById('btns');
+const otherBtn = document.getElementById('otherBtn');
+let counter = 0;
 
-// Event listener per il pulsante di incremento
-incrementBtn.addEventListener("click", () => {
-    if (counter < 9999) {
-        counter++;
+function displayBtn() {
+    const minus = document.createElement('button');
+    minus.innerHTML = '-';
+    minus.id = 'decrement';
+    minus.className = 'btn';
+    btns.appendChild(minus);
+
+    const plus = document.createElement('button');
+    plus.innerHTML = '+';
+    plus.id = 'increment';
+    plus.className = 'btn';
+    btns.appendChild(plus);
+
+    const decBtn = document.createElement('button');
+    decBtn.innerHTML = 'REM';
+    decBtn.className = 'btn';
+    decBtn.id = 'remValue';
+    otherBtn.appendChild(decBtn);
+
+    const incBtn = document.createElement('button');
+    incBtn.innerHTML = 'ADD';
+    incBtn.className = 'btn';
+    incBtn.id = 'addValue';
+    otherBtn.appendChild(incBtn);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    displayBtn();
+
+    // Event Delegation per i pulsanti di incremento, decremento, aggiunta e rimozione, reset e input di aggiunta
+    document.addEventListener("click", (event) => {
+        const target = event.target;
+
+        if (target.id === "increment") {
+            if (counter < 9999) {
+                counter++;
+            }
+        } else if (target.id === "decrement") {
+            if (counter > 0) {
+                counter--;
+            }
+        } else if (target.id === "addValue") {
+            const currentValue = counter;
+            const numberToAdd = Number(inputNumber.value);
+            const newValue = isNaN(numberToAdd) ? currentValue : currentValue + numberToAdd;
+            counter = newValue > 9999 ? 9999 : newValue;
+        } else if (target.id === "remValue") {
+            const currentValue = counter;
+            const numberToRem = Number(inputNumber.value);
+            const newValue = isNaN(numberToRem) ? currentValue : currentValue - numberToRem;
+
+            if (newValue < 0) {
+                return;
+            }
+
+            counter = newValue;
+        } else if (target.id === "reset") {
+            counter = 0;
+        }
+
         count.textContent = counter.toString().padStart(4, '0');
-    }
-});
+    });
 
-// Event listener per il pulsante di decremento
-decrementBtn.addEventListener("click", () => {
-    if (counter > 0) {
-        counter--;
-        count.textContent = counter.toString().padStart(4, '0');
-    }
-});
-
-// Event listener per il pulsante di decremento con gestione della prevenzione dell'evento
-decrementBtn.addEventListener("click", (event) => {
-    if (counter === 0) {
-        event.preventDefault();
-    }
-});
-
-
-// Event listener per il pulsante che permette il reset del counter
-resetBtn.addEventListener("click", () => {
-    counter = 0;
-    count.textContent = counter.toString().padStart(4, '0');
-  });
-
-// Event listener per il pulsante di aggiunta
-addButton.addEventListener('click', () => {
-    const currentValue = counter;
-    const numberToAdd = Number(inputNumber.value);
-    const newValue = isNaN(numberToAdd) ? currentValue : currentValue + numberToAdd;
-    counter = newValue > 9999 ? 9999 : newValue;
-    count.textContent = counter.toString().padStart(4, '0');
-});
-
-// Event listener per l'input del pulsante di aggiunta con gestione del valore non numerico
-addButton.addEventListener('input', () => {
-    if (isNaN(parseInt(inputNumber.value))) {
-        count.textContent = 0000;
-    }
-});
-
-// Event listener per il pulsante di rimozione
-remButton.addEventListener('click', () => {
-    const currentValue = counter;
-    const numberToRem = Number(inputNumber.value);
-    const newValue = isNaN(numberToRem) ? currentValue : currentValue - numberToRem;
-
-    if (newValue < 0000) {
-        return;
-    }
-
-    counter = newValue;
-    count.textContent = counter.toString().padStart(4, '0');
-});
-
-// Event listener per l'input del pulsante di rimozione con gestione del valore non numerico
-remButton.addEventListener('input', () => {
-    if (isNaN(parseInt(inputNumber.value))) {
-        count.textContent = 0000;
-    }
+    // Event listener per l'input del pulsante di aggiunta con gestione del valore non numerico
+    inputNumber.addEventListener('input', () => {
+        if (isNaN(parseInt(inputNumber.value))) {
+            count.textContent = "0000";
+        }
+    });
 });
